f9debf0e163bebced7012c90e7cf8fa1b11372fe
F1 race car team <ucichemecars@gmail.com>
2017-09-07 01:38:22 -0700

    Refactor the code

--- Overall Code/National 2017 Overall/Nationa_2017_Overall/Nationa_2017_Overall.ino
+++ Overall Code/National 2017 Overall/Nationa_2017_Overall/Nationa_2017_Overall.ino
@@ -1,8 +1,19 @@
-  #include "DualMC33926MotorShield.h"
-#include <PololuWheelEncoders.h>
+/*
+*  Written for use in National Competition 2017 with hydrogen fuel cell and
+*  Sodium Thiosulfate Reaction
+*/
+
+#include "DualMC33926MotorShield.h"//Pololu shield library, available on ChemE car github account
+#include <PololuWheelEncoders.h>//Pololu encoder for the motors, available on ChemE car github account
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_TSL2591.h"
+#include <Adafruit_Sensor.h>//Adafruit unified sensor library
+#include "Adafruit_TSL2591.h"//TSL2591 library
+
+#define Motor_Tick_per_rotation 3591.84
+#define motor1_speed 393//adjust for speed of motor 1
+#define motor2_speed 372//adjust for speed of motor 2
+#define car_speed 0.54//speed of the car
+
 DualMC33926MotorShield md;
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
 void stopIfFault()
@@ -23,7 +34,7 @@ void displaySensorDetails(void)
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" lux");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" lux");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");
   Serial.println("------------------------------------");
   Serial.println("");
   delay(500);
@@ -41,7 +52,7 @@ void configureSensor(void)
   //tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
   tsl.setGain(TSL2591_GAIN_HIGH);
   //tsl.setGain(TSL2591_GAIN_MAX); // 428x gain
-  
+
   // Changing the integration time gives you a longer time over which to sense light
   // longer timelines are slower, but are good in very low light situtations!
   tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
@@ -51,7 +62,7 @@ void configureSensor(void)
   //tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
   //tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
 
-  /* Display the gain and integration time for reference sake */  
+  /* Display the gain and integration time for reference sake */
   Serial.println("------------------------------------");
   Serial.print  ("Gain:         ");
   tsl2591Gain_t gain = tsl.getGain();
@@ -71,7 +82,7 @@ void configureSensor(void)
       break;
   }
   Serial.print  ("Timing:       ");
-  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
+  Serial.print((tsl.getTiming() + 1) * 100, DEC);
   Serial.println(" ms");
   Serial.println("------------------------------------");
   Serial.println("");
@@ -82,28 +93,28 @@ void configureSensor(void)
     Program entry point for the Arduino sketch
 */
 /**************************************************************************/
-void setup(void) 
+void setup(void)
 {
 Serial.begin(115200);
-  
+
   Serial.println("Starting Adafruit TSL2591 Test!");
-  
-  if (tsl.begin()) 
+
+  if (tsl.begin())
   {
     Serial.println("Found a TSL2591 sensor");
-  } 
-  else 
+  }
+  else
   {
     Serial.println("No sensor found ... check your wiring?");
     while (1);
   }
-    
+
   /* Display some basic information on this sensor */
   displaySensorDetails();
-  
+
   /* Configure the sensor */
   configureSensor();
-    
+
   // Now we're ready to get readings ... move on to loop()!
 
   Serial.println("Dual MC33926 Motor Shield");
@@ -112,7 +123,7 @@ Serial.begin(115200);
 }
 void simpleRead(void)
 {
-  // Simple data read example. Just read the infrared, fullspecrtrum diode 
+  // Simple data read example. Just read the infrared, fullspecrtrum diode
   // or 'visible' (difference between the two) channels.
   // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
   //uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
@@ -151,14 +162,14 @@ void advancedRead(void)
 /**************************************************************************/
 void unifiedSensorAPIRead(void)
 {
-  /* Get a new sensor event */ 
+  /* Get a new sensor event */
   sensors_event_t event;
   tsl.getEvent(&event);
- 
+
   /* Display the results (light is measured in lux) */
   Serial.print("[ "); Serial.print(event.timestamp); Serial.print(" ms ] ");
   if ((event.light == 0) |
-      (event.light > 4294966000.0) | 
+      (event.light > 4294966000.0) |
       (event.light <-4294966000.0))
   {
     /* If event.light = 0 lux the sensor is probably saturated */
@@ -174,117 +185,122 @@ void unifiedSensorAPIRead(void)
 
 void loop()
 {
- /*********Stopping code************/
- static float total2;
-  static float total1;
-  static int f2=0;// flag for marking the car starts running physically 
-  static int m1=393;//adjust for speed of motor 1
-  static int m2=372;// adjust for speed of motor 2
-  static int count6=0;//count the amount of times the sensor has detected 63534
-  static int countd=0;//count the amount of times the solution has gone dark
+  //Sensor Variables
   uint32_t lum = tsl.getFullLuminosity();
   uint16_t ir, full;
+
+  //Motor Variables
   static float distance=0;// distance the car will travel at the given speed in meters
-  static float sp=0.54; // The speed of the car in m/s
-  static unsigned long t0=0; //the time that liquid was stabilized
-  static unsigned long t1=0; //the time that the liquid went dark
-  static unsigned long t00=0;
-  static unsigned long time2=0;
-  static unsigned long time1=0; //time it takes for the liquid to go from injected to dark
-  static unsigned long totalRuntime=0;
+  static boolean car_moved=false; // flag to indicate phase, 1 for liquid injected, 2 for stabilized, 3 for finished reaction.
+  static float total1;//total amount of rotation for motor 1
+  static unsigned long totalRuntime=0;//amount of time the car has been running
   static unsigned long tRun1=0;//start of run time
   static unsigned long tRun2=0;//end of run time
-  static int f1=0; // flag to indicate phase, phase 0 is the default where nothing has happened. 
+
+  //Reaction variables
+  static int count6=0;//count the amount of times the sensor has detected 63534
+  static int countd=0;//count the amount of times the solution has gone dark
+  static unsigned long t_stabilized=0; //the time that liquid was stabilized
+  static unsigned long t_dark=0; //the time that the liquid went dark
+  static unsigned long t_injected=0;//mark when liquid was injected
+  static unsigned long dur_injected=0;//time since injection
+  static unsigned long dur_injected_to_stabilized=0; //time it takes for the liquid to go from injected to stabilized
+  static int flag_reaction=0;//flags for indicating the phase of the reaction
+
+  /*Light Sensor measurement*/
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  unsigned int a=tsl.calculateLux(full, ir);
-  
-  if(a<40000 && millis()>=200 && f1==0)
+  unsigned int light_level=tsl.calculateLux(full, ir);
+
+  /*********************Reaction Event Detection Code************************/
+
+  //Phase 1: Liquid was not injected
+  if(light_level<40000 && millis()>=200 && flag_reaction==0)
   {
-    f1=1;//liquid has been injected. 
-    t00=millis();
+    flag_reaction=1;//liquid has been injected.
+    t_injected=millis();
   }
-  if(f1==1 && a>40000)
+
+  //Phase 2: Injection was dectected, the code will now search for stabilization
+  if(flag_reaction==1 && light_level>40000)
   {
     if(count6==0)
     {
-      t0=millis();
+      t_stabilized=millis();//mark the beginning of stabilization, will be reset if unstable
     }
     if(count6>5)
     {
-       f1=2;//liquid has been stabilized
+       flag_reaction=2;//liquid has been stabilized
     }
      ++count6;
   }
-  if(f1==1 && a<40000)
+  if(flag_reaction==1 && light_level<40000)
   {
-    t0=0;//reset variables if the stabilization was not continuous
+    t_stabilized=0;//reset variables if the stabilization was not continuous
     count6=0;
   }
-  if(f1==2 && a<40000)
+
+  //Phase 3: Stabilization reached, searching for the darkening of the solution
+  if(flag_reaction==2 && light_level<40000)
   {
     //liquid has turned dark
     if(countd==0)
     {
-    t1=millis();
+    t_dark=millis();
     }
-    time1=t1-t0;//time since stabilization
-    time2=t1-t00;//time since injection
-    if (time1>10000 && countd>5)
+    dur_injected_to_stabilized=t_dark-t_stabilized;//time since stabilization
+    dur_injected=t_dark-t_injected;//time since injection
+    if (dur_injected_to_stabilized>10000 && countd>5)
     {
-      f1=3;
-      totalRuntime=time1;
+      flag_reaction=3;
+      totalRuntime=dur_injected_to_stabilized;
     }
     ++countd;
   }
-  
-  Serial.print(millis()); Serial.print("          "); 
-  Serial.print(a); Serial.print("             "); 
-  Serial.print(t0);  Serial.print("                                 "); 
-  Serial.print(t1); Serial.print("                              "); 
-  Serial.print(time1);  Serial.print("    ms        ");
-  //Serial.print(time2); 
-  distance=(sp*time1/1000.0)-0.322;
+
+  /*Diagnostic and Prediction Code*/
+  Serial.print(millis()); Serial.print("          ");
+  Serial.print(light_level); Serial.print("             ");
+  Serial.print(t_stabilized);  Serial.print("                                 ");
+  Serial.print(t_dark); Serial.print("                              ");
+  Serial.print(dur_injected_to_stabilized);  Serial.print("    ms        ");
+
+  distance=(car_speed*dur_injected_to_stabilized/1000.0)-0.322;//Formula for calculating run distance in meters. This is only a prediction
+
   Serial.print(distance); Serial.println("    m   ");
-  
-  /***********Motor code*************/
-  /*
-  if(f1==3)
-  {
-    md.setM1Speed(0);
-    md.setM2Speed(0);
-  }
-  else
-  {
-    md.setM1Speed(380);
-    md.setM2Speed(400);
-  }
-  */
-  /**********Alternate motor code that used motor run time*************/
-  
-  
+
+
+  /************************Motor Control Code***********************/
+
+  //Encoder Measurement
   int i=PololuWheelEncoders::getCountsAndResetM1();
-  total1=total1+abs(i/3591.84);
-  if(total1==0 && tRun1==0 && totalRuntime==0)
+  total1=total1+abs(i/Motor_Tick_per_rotation);//Amount of rotation on Motor 1, used only for sensing whether the car moved.
+
+  //Determining the phase of movement
+
+  if(!total1 && !tRun1 && !totalRuntime)
   {
-    md.setM1Speed(m1);
-    md.setM2Speed(m2);
+    md.setM1Speed(motor1_speed);
+    md.setM2Speed(motor2_speed);
     Serial.println("It has not run");
   }
-  if(total1>0.1 && f2==0)
+
+  if(total1>0.1 && !car_moved)
   {
-    f2=1;
+    car_moved=!car_moved;
     tRun1=millis();
-    md.setM1Speed(m1);
-    md.setM2Speed(m2);
+    md.setM1Speed(motor1_speed);
+    md.setM2Speed(motor2_speed);
     Serial.println("It started running");
   }
-  if (tRun1>0 && totalRuntime==0)
+
+  if (tRun1>0 && !totalRuntime)
   {
-    md.setM1Speed(m1);
-    md.setM2Speed(m2);
+    md.setM1Speed(motor1_speed);
+    md.setM2Speed(motor2_speed);
     Serial.println("Stopping Reaction has not finished");
   }
+
   if (tRun1>0 && totalRuntime>0)
   {
     if((millis()-tRun1)>totalRuntime)
@@ -293,19 +309,20 @@ void loop()
     md.setM2Speed(0);
     Serial.println("Stopping Reaction has finished run time has reached");
     }
-    else if(millis()-tRun1<totalRuntime)
+    else if((millis()-tRun1)<totalRuntime)
     {
-    md.setM1Speed(m1);
-    md.setM2Speed(m2);
+    md.setM1Speed(motor1_speed);
+    md.setM2Speed(motor2_speed);
     Serial.println("Stopping Reaction has finished but run time has not reached");
     }
     else
     {
       Serial.println("Outlier");
       md.setM1Speed(0);
-    md.setM2Speed(0);
+      md.setM2Speed(0);
     }
   }
+
   Serial.print(total1); Serial.print("    rotation    ");
   Serial.println(tRun1);
 }
